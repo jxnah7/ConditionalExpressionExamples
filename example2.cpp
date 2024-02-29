1ca558f3d2234ddc8df4e17e8bcec73ebbc93458
Jonah Barrera <116769667+jxnah7@users.noreply.github.com>
2024-02-29 15:04:11 -0600

    Create example2.cpp

---
+++ example2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int month = 0;
+    string monthname, season;
+//Get the month number:
+    cout << "Please enter a month number: ";
+    cin >> month;
+    
+    if (month == 1)
+        monthname = "January";
+    else if (month == 2)
+        monthname = "Februrary";
+    else if (month == 3)
+        monthname = "March";
+    else if (month == 4)
+        monthname = "April";
+    else if (month == 5)
+        monthname = "May";
+    else if (month == 6)
+        monthname = "June";
+    else if (month == 7)
+        monthname = "July";
+    else if (month == 8)
+        monthname = "August";
+    else if (month == 9)
+        monthname = "September";
+    else if (month == 10)
+        monthname = "October";
+    else if (month == 11)
+        monthname = "November";
+    else if (month == 12)
+        monthname = "December";
+    else
+        monthname = "an invalid entry";
+        
+    cout << "\nYou entered " << month << ", which corresponds to " << monthname << endl << endl;
+    
+    return 0;
+}
